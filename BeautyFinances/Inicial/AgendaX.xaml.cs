5a9bc814093730279964856c3f9084334e6fc1a2
Maxwell Schner <schnermaxwell@gmail.com>
2018-09-26 21:36:49 -0300

    Validação da agenda

--- BeautyFinances/Inicial/AgendaX.xaml.cs
+++ BeautyFinances/Inicial/AgendaX.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Inicial.Models;
+using System;
 using System.Linq;
 using System.Windows;
 
@@ -18,7 +19,45 @@ namespace Inicial
             {
                 cbFuncionario.ItemsSource = db.Funcionarios.ToList();
                 cbCliente.ItemsSource = db.Clientes.ToList();
+                cbServico.ItemsSource = db.Servicos.ToList();
             }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (VerificaAgendamento())
+            {
+                using (var db = new ModelContext())
+                {
+                    db.Horarios.Add(new Horarios
+                    {
+                        idFuncionario = (int)cbFuncionario.SelectedValue,
+                        idCliente = (int)cbCliente.SelectedValue,
+                        IdServico = (int)cbServico.SelectedValue,
+
+                    });
+                }
+            }
+        }
+
+        private bool VerificaAgendamento()
+        {
+            if (cbFuncionario.SelectedIndex == -1)
+            {
+                MessageBox.Show("Selecione um funcionário");
+                return false;
+            }
+            if (cbCliente.SelectedIndex == -1)
+            {
+                MessageBox.Show("Selecione um cliente");
+                return false;
+            }
+            if (cbServico.SelectedIndex == -1)
+            {
+                MessageBox.Show("Selecione um serviço");
+                return false;
+            }
+            return true;
         }
     }
 }
